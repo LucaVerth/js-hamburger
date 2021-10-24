c326d7e29226268f39bea7c3676ad0eeb96ccc16
LucaVerth <luca.verthuy@gmail.com>
2021-10-24 18:50:59 +0200

    Updated code with right method // Used plain JS instead of Jquery

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // display hamburger menu
 
+// Jquery
+/* 
 $(document).ready(function () {
   $(".fa-bars").click(function () {
     $(".hamburger-menu").show();
@@ -9,3 +11,18 @@ $(document).ready(function () {
     $(".hamburger-menu").hide();
   }); // close menu
 });
+*/
+
+
+// plain javascript
+const openIcon = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('.hamburger-menu'); 
+const closeIcon = document.querySelector('.close');
+
+openIcon.onclick = function (){
+  navMenu.style.display = 'block';
+};
+
+closeIcon.onclick = function (){
+  navMenu.style.display = 'none';
+};
